d09f2146eec26fa586a8a3c7837185c9c2cb572b
Tobias Argain <tobiasaargain@gmail.com>
2022-05-20 12:54:03 -0300

    boton contacto y diseño

--- js/main.js
+++ js/main.js
@@ -29,9 +29,9 @@ const mostrarProductos = ()=>{
 
 const mostrarModelos = () => {
 
-    contenedorModeloUno.innerHTML = `<img src="${kayaks[0].imagen}"> <img src="${kayaks[0].imagen2}"> <img src="${kayaks[0].imagen3}"> <img src="${kayaks[0].imagen4}"></img> <img src="${kayaks[0].imagen5}"> <img src="${kayaks[0].imagen6}">`;
+    contenedorModeloUno.innerHTML = `<img src="${kayaks[0].imagen}"> <img src="${kayaks[0].imagen2}"> <img src="${kayaks[0].imagen3}"> <img src="${kayaks[0].imagen4}"></img> <img src="${kayaks[0].imagen5}"> <img src="${kayaks[0].imagen6}" class="imgConInfo">`;
 
-    contenedorModeloDos.innerHTML = `<img src="${kayaks[1].imagen}"> <img src="${kayaks[1].imagen2}"> <img src="${kayaks[1].imagen3}"> <img src="${kayaks[1].imagen4}"></img> <img src="${kayaks[1].imagen5}"> <img src="${kayaks[1].imagen6}">`;
+    contenedorModeloDos.innerHTML = `<img src="${kayaks[1].imagen}"> <img src="${kayaks[1].imagen2}"> <img src="${kayaks[1].imagen3}"> <img src="${kayaks[1].imagen4}"></img> <img src="${kayaks[1].imagen5}"> <img src="${kayaks[1].imagen6}" class="imgConInfo">`;
 }
 
 
